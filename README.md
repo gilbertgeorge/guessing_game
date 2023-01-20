f66e056fab28bf08bf9144983fe931931bd5233e
gilbertgeorge <george@grgilbert.com>
2023-01-20 15:13:25 -0500

    game working

---
+++ README.md
@@ -0,0 +1,7 @@
+# RUST documentation is amazing ❤️🦀
+https://doc.rust-lang.org/
+
+## Simple stupid guessing game
+
+### Following:
+https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
-    println!("add a line");
-    println!("test");
+    let min = 1;
+    let max = 100;
+
+    println!("Guess the number in range {min} to {max}!");
+    let secret_number = rand::thread_rng().gen_range(min..=max);
+    // println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line.");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number!");
+                continue;
+            },
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
